cae9f80c964fc98369df0f7f0ca4d9fbcb008cab
pnzaou <perrinemmanuelnzaou@gmail.com>
2025-10-09 15:55:50 +0000

    MAJ5

--- src/app.ts
+++ src/app.ts
@@ -15,10 +15,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(helmet());
 app.use(compression());
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
